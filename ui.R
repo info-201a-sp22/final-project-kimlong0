157607eb6066b22b302d0991634884594583271d
kimlong0 <kimlong0@uw.edu>
2022-05-31 23:00:57 -0700

    fix tab3 not showing

--- ui.R
+++ ui.R
@@ -68,16 +68,8 @@ main_plot_tab2 <- fluidRow(
   plotlyOutput(outputId = "top_occupation_bar_chart")
 )
 
-main_panel_plot_tab3 <- mainPanel(
-  fluidPage(
-    plotOutput(outputId = "top_pay_diff_tab3")
-  ))
-
-tab3 <- tabPanel(
-  "woo", 
-  sidebarLayout(
-    main_panel_plot_tab3
-  )
+main_panel_plot_tab3 <- fluidRow(
+  plotOutput(outputId = "top_pay_diff_tab3")
 )
 
 # page 1 
@@ -99,10 +91,11 @@ tab2 <- tabPanel(
 )
 
 # page 3
-
-
 tab3 <- tabPanel(
-  "Chart 3"
+  "woo", 
+  fluidPage(
+    main_panel_plot_tab3
+  )
 )
 
 # Conclusion page
